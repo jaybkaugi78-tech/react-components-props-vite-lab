45f0df529ace6a59a61bbc060c8924b351ff9e42
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-05-01 22:52:02 +0300

    completed the labs and updated from js to jsx due to error

---
+++ src/blog.js
@@ -0,0 +1,21 @@
+const blog = {
+  name: "Jay's Blog",
+  image: "https://via.placeholder.com/150",
+  about: "Welcome to my blog! I write about tech, coding, and alot about carsc.",
+  posts: [
+    {
+      id: 1,
+      title: "My First Post",
+      date: "May 1, 2026",
+      preview: "This is my first blog post!"
+    },
+    {
+      id: 2,
+      title: "React is Cool",
+      date: "May 2, 2026",
+      preview: "React makes building UIs easy."
+    }
+  ]
+};
+
+export default blog;
